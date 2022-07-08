f0d1d5dcf4bd33fbf72cb81c4105d72edc048049
Cyril <cyril.grelier@outlook.fr>
2022-07-08 17:54:06 +0200

    update instances and prepare run

--- scripts/generator_to_eval_mcts.py
+++ scripts/generator_to_eval_mcts.py
@@ -4,19 +4,26 @@ Generate to_eval file which list all run to perform for MCTS
 to split :
     split -l 1000 -d to_eval_mcts to_eval_mcts
 
-instance                  // i,instance
-problem                   // p,problem
-method                    // m,method
-time_limit                // t,time_limit
-rand_seed                 // r,rand_seed
-target                    // T,target
-nb_max_iterations         // n,nb_max_iterations
-initialization_str        // I,initialization
-nb_iter_local_search      // N,nb_iter_local_search
-max_time_local_search     // M,max_time_local_search
-coeff_exploi_explo        // c,coeff_exploi_explo
-local_search_str          // l,local_search
-simulation_str            // s,simulation
+Parameters : 
+    p,problem
+    i,instance
+    m,method
+    r,rand_seed
+    T,target
+    u,use_target
+    b,objective
+    t,time_limit
+    n,nb_max_iterations
+    I,initialization
+    N,nb_iter_local_search
+    M,max_time_local_search
+    c,coeff_exploi_explo
+    l,local_search
+    s,simulation
+    O,O_time
+    P,P_time
+    o,output_file
+
 """
 
 import os
@@ -34,28 +41,28 @@ def get_target(instance: str):
     print(f"instance {instance} not found in instances/best_scores_wvcp.txt")
 
 
-# instance  # i,instance
+# i,instance
 with open("all_instances.txt", "r", encoding="UTF8") as file:
     instances = [line[:-1] for line in file.readlines()]
 
-problem = "wvcp" # "wvcp" "gcp" # p, problem
-method = "mcts"  # m,method
-time_limit = 3600  # t,time_limit
-# rand_seed  # r,rand_seed
+
+problem = "wvcp"  # gcp wvcp
+method = "mcts"
+
 rand_seeds = list(range(20))
-target = 0  # T,target
-nb_max_iterations = 9000000000000000000  # n,nb_max_iterations
 
-nb_iter_local_search = 500  # N,nb_iter_local_search
-
-max_time_local_search = 3600  # M,max_time_local_search
-
+target = 0
+use_target = "false"
+objective = "optimality"  # optimality reached
+time_limit = 3600
+nb_max_iterations = 9000000000000000000
 initializations = [
     # "random",
     # "constrained",
     "deterministic",
-]  # I,initialization
-
+]
+nb_iter_local_search = 9000000000000000000
+max_time_local_search = -1
 coeff_exploi_explo = [
     # "0",
     # "0.25",
@@ -66,26 +73,27 @@ coeff_exploi_explo = [
     # "1.5",
     # "1.75",
     # "2",
-]  # c,coeff_exploi_explo
-
+]
 local_searchs = [
     # "none",
     # "tabu_col",
     # "hill_climbing",
-    # "afisa_original",
-    "afisa",
+    "afisa_original",
+    # "afisa",
     "tabu_weight",
     "redls",
     "ilsts",
-]  # l,local_search
-
+]
 simulations = [
     # "greedy",
     "local_search",
     # "fit",
     # "depth",
     # "depth_fit",
-]  # s,simulation
+]
+O_time = 0
+P_time = 0.02
+
 
 output_directory = "output_mcts_local_search_deterministic"
 
@@ -95,9 +103,8 @@ for initialization in initializations:
         for local_search in local_searchs:
             for simulation in simulations:
                 os.mkdir(f"{output_directory}/{local_search}")
-                # os.mkdir(f"{output_directory}/{initialization}{coeff}/{local_search}/{simulation}")
 
-with open("to_eval_mcts_ls", "w", encoding="UTF8") as file:
+with open("to_eval_mcts", "w", encoding="UTF8") as file:
     for initialization in initializations:
         for coeff in coeff_exploi_explo:
             for local_search in local_searchs:
@@ -107,19 +114,23 @@ with open("to_eval_mcts_ls", "w", encoding="UTF8") as file:
                         for rand_seed in rand_seeds:
                             file.write(
                                 f"./gc_wvcp "
-                                f" -p {problem}"
-                                f" -i {instance}"
-                                f" -m {method}"
-                                f" -t {time_limit}"
-                                f" -r {rand_seed}"
-                                f" -T {target}"
-                                f" -n {nb_max_iterations}"
-                                f" -I {initialization}"
-                                f" -N {nb_iter_local_search}"
-                                f" -M {max_time_local_search}"
-                                f" -c {coeff}"
-                                f" -l {local_search}"
-                                f" -s {simulation}"
-                                f" -o ../{output_directory}/{local_search}/{instance}_{rand_seed}.csv"
+                                f" --problem {problem}"
+                                f" --instance {instance}"
+                                f" --method {method}"
+                                f" --rand_seed {rand_seed}"
+                                f" --target {target}"
+                                f" --use_target {use_target}"
+                                f" --objective {objective}"
+                                f" --time_limit {time_limit}"
+                                f" --nb_max_iterations {nb_max_iterations}"
+                                f" --initialization {initialization}"
+                                f" --nb_iter_local_search {nb_iter_local_search}"
+                                f" --max_time_local_search {max_time_local_search}"
+                                f" --coeff_exploi_explo {coeff}"
+                                f" --local_search {local_search}"
+                                f" --simulation {simulation}"
+                                f" --O_time {O_time}"
+                                f" --P_time {P_time}"
+                                f" --output_file ../{output_directory}/{local_search}/{instance}_{rand_seed}.csv"
                                 "\n"
                             )
