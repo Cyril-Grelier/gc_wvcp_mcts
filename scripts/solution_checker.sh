dc0eb0022b0a7d8a533af30abce9ca7b52000fdf
Cyril <cyril.grelier@outlook.fr>
2022-07-09 18:01:46 +0200

    update solution checker

--- scripts/solution_checker.sh
+++ scripts/solution_checker.sh
@@ -8,12 +8,12 @@ source ../venv/bin/activate
 # get final instance, score and solution from given file
 old_ifs=$IFS
 IFS=,
-read -r instance score solution < <(csvcut -c instance,score,solution "$1" | tail -n 1)
+read -r instance problem score solution < <(csvcut -c instance,problem,score,solution "$1" | tail -n 1)
 IFS=$old_ifs
 
 cd ../instances || exit
 
-if ! python3 check_solution.py "$instance" "$score" "$solution";
+if ! python3 check_solution.py "$instance" "$problem" "$score" "$solution";
 then
     echo "Error : $1"
 fi
